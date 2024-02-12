1337f033a6ce194818134cbc21e7d6b3c23220d5
Darshan Begani <saritabegani76@gmail.com>
2024-02-12 18:30:36 +0530

    for loop without Parameters

---
+++ conditions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+for(;;){//for loop without intialization , conditionn and updation and it is a infinite loop bcz there is no condition/statement for the breaking of the loop. Agar i 6 ho gya to if case nhi chalega par ye loop ma rhega infinitly.
+    if(i<5){
+        cout<<"Apple";
+        i=i+1;
+    }
+}
+
+    return 0;
+}
